7970d4331cdb1e93dd42d13aa51227b7e51d48a1
syedibrahim0325-cloud <syedibrahim0325@gmail.com>
2026-02-21 14:04:26 +0530

    Problem2 commited

---
+++ Problem2.java
@@ -0,0 +1,11 @@
+public class Problem2
+{
+public static void main(String args[])
+{
+double phy= 95.0;
+double math= 94.0;
+double chem= 96.0;
+double avg= (phy+math+chem)/3;
+System.out.println("Average marks is:" + avg);
+}
+}
